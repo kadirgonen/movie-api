bf5b9f67fdf36a041c2f1c3a6f1113018038fc26
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-08-24 15:10:04 +0300

    Add to movie model
    Add to movie model

---
+++ app/models/movie.go
@@ -0,0 +1,11 @@
+package model
+
+import "gorm.io/gorm"
+
+type Movie struct {
+	gorm.Model
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+}
