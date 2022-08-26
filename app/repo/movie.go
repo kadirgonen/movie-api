5a8b420b5734f8fcbb53f62ecd434b2597095812
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-08-26 12:23:37 +0300

    Create to movie repo
    Create to movie repo

--- app/repo/movie.go
+++ app/repo/movie.go
@@ -13,16 +13,16 @@ type MovieRepository struct {
 	db *gorm.DB
 }
 
-func NewCategoryRepository(db *gorm.DB) *MovieRepository {
+func NewMovieRepository(db *gorm.DB) *MovieRepository {
 	return &MovieRepository{db: db}
 }
 
-func (c *MovieRepository) Migrate() {
-	c.db.AutoMigrate(&Movie{})
+func (m *MovieRepository) Migrate() {
+	m.db.AutoMigrate(&Movie{})
 }
-func (c *MovieRepository) Create(mv *Movie) (*Movie, error) {
+func (m *MovieRepository) Create(mv *Movie) (*Movie, error) {
 	zap.L().Debug("movie.repo.create", zap.Reflect("movie", mv.Name))
-	if err := c.db.Create(&mv).Error; err != nil {
+	if err := m.db.Create(&mv).Error; err != nil {
 		zap.L().Error("movie.repo.create failed to create movie", zap.Error(err))
 		return nil, err
 	}
@@ -31,10 +31,10 @@ func (c *MovieRepository) Create(mv *Movie) (*Movie, error) {
 
 // Update take movie id. It can be uses without id with Save. But it creates different movie id's. Here it can be uses with Save() without Where() conditions,
 // because id is implemented movie. Here I would like to differentiate usage practices.
-func (c *MovieRepository) Update(mv *Movie, id int) (*Movie, error) {
+func (m *MovieRepository) Update(mv *Movie, id int) (*Movie, error) {
 	zap.L().Debug("movie.repo.update", zap.Reflect("movie", mv))
-	mv.ID = id
-	if err := c.db.Where("id=?", id).Updates(&mv).Error; err != nil {
+	mv.ID = &id
+	if err := m.db.Where("id=?", id).Updates(&mv).Error; err != nil {
 		zap.L().Error("movie.repo.update failed to update movie", zap.Error(err))
 		return nil, err
 	}
@@ -42,25 +42,47 @@ func (c *MovieRepository) Update(mv *Movie, id int) (*Movie, error) {
 	return mv, nil
 
 }
-func (c *MovieRepository) Upload(movies *MovieList) (int, string, error) {
+
+// Delete helps user to delete movie. Here is hard-delete. If you would like to change it soft-delete, just remove "Unscoped()".
+func (m *MovieRepository) Delete(id int) (bool, error) {
+	zap.L().Debug("movie.repo.delete", zap.Reflect("query", id))
+	if err := m.db.Unscoped().Where("id=?", id).Delete(&Movie{}).Error; err != nil {
+		zap.L().Error("movie.repo.delete failed to delete movie", zap.Error(err))
+		return false, err
+	}
+	return true, nil
+}
+
+// CheckMovie helps user to check product is exist or not
+func (m *MovieRepository) CheckMovie(id int) (bool, error) {
+	var mv *Movie
+	var exists bool = false
+	zap.L().Debug("movie.repo.checkproduct")
+	r := m.db.Where("id=?", id).Limit(1).Find(&mv)
+	if r.RowsAffected > 0 {
+		exists = true
+	}
+	return exists, nil
+}
+func (m *MovieRepository) Upload(movies *MovieList) (int, string, error) {
 	var count int64
-	err := c.db.Create(&movies).Count(&count).Error
+	err := m.db.Create(&movies).Count(&count).Error
 	return int(count), os.Getenv("CREATE_FILE"), err
 }
 
-func (c *MovieRepository) GetAll() (*MovieList, error) {
+func (m *MovieRepository) GetAll() (*MovieList, error) {
 	var movies MovieList
-	err := c.db.Find(&movies).Error
+	err := m.db.Find(&movies).Error
 	if err != nil {
 		return nil, err
 	}
 	return &movies, nil
 }
 
-func (c *MovieRepository) GetAllMoviesWithPagination(pag Pagination) ([]Movie, int, error) {
+func (m *MovieRepository) GetAllMoviesWithPagination(pag Pagination) ([]Movie, int, error) {
 	var movies []Movie
 	var count int64
-	err := c.db.Offset(int(pag.Page) - 1*int(pag.PageSize)).Limit(int(pag.PageSize)).Find(&movies).Count(&count).Error
+	err := m.db.Offset(int(pag.Page) - 1*int(pag.PageSize)).Limit(int(pag.PageSize)).Find(&movies).Count(&count).Error
 	if err != nil {
 		return nil, -1, err
 	}
